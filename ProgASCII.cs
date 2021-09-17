4755f878cb2dacfc0ad8de03fe507351876f97d7
NikitaTroyes <89306099+NikitaTroyes@users.noreply.github.com>
2021-09-17 17:27:25 +0200

    Create ProgASCII

---
+++ ProgASCII.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progASCII_quête3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        { 
+
+            string value = "abcdefghijklmnopqrstuvwxyz";
+
+        byte[] asciiBytes = Encoding.ASCII.GetBytes(value);
+
+
+    }
+    }
+}
